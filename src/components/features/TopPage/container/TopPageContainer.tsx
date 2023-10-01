bbcd69778e6b7081071eba3d9901dd8315a8abd6
motoki_yuto <motoki.youto293@gmail.com>
2023-10-01 12:23:49 +0900

    feat: initialize TopPage

---
+++ src/components/features/TopPage/container/TopPageContainer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { TopPage } from "../../../pages/TopPage";
+
+export const TopPageContainer: React.FC = () => {
+  return (
+    <>
+      <TopPage />
+    </>
+  );
+};
---
+++ src/components/pages/TopPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { TopPageContainer } from "../features/TopPage/container/TopPageContainer";
+
+export const TopPage: React.FC = () => {
+  return (
+    <>
+      <TopPageContainer />
+    </>
+  );
+};
